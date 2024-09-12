47e8bb42b82232ed9a916c48657ac75d7ebf59d2
Andre Lucas <andrelucastic@gmail.com>
2024-09-12 12:52:58 +0100

    add hpa

---
+++ infraascode/metrics-server/metrics.sh
@@ -0,0 +1,2 @@
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+helm upgrade --install metrics-server metrics-server/metrics-server --namespace kube-system --set args="{--kubelet-insecure-tls}"
